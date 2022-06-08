cf04af74b2fa0e11f4c9b1d05a6b2ef131cd3ca3
Jioh In <jioh.in@prismsus.org>
2022-06-08 16:15:23 -0400

    Create React Project

--- src/components/realtimeDataMobile/index.js
+++ src/components/realtimeDataMobile/index.js
@@ -50,7 +50,7 @@ export class RealtimeDataMobile extends React.Component {
                                     ): (
                                         machine.status === "pause" ? (
                                             <div>
-                                                <img className={cName} alt = "orange dryer" src={require("../img/purple_dryer.jpg")} /> 
+                                                <img className={cName} alt = "orange dryer" src={require("../img/orange_dryer.jpg")} /> 
                                             </div>
                                         ): (
                                             <div>
@@ -79,7 +79,7 @@ export class RealtimeDataMobile extends React.Component {
                                     ): (
                                         machine.status === "pause" ? (
                                             <div>
-                                                <img className={cName} alt = "orange washer" src={require("../img/purple_washer.jpg")} /> 
+                                                <img className={cName} alt = "orange washer" src={require("../img/orange_washer.jpg")} /> 
                                             </div>
                                         ): (
                                             <div>
